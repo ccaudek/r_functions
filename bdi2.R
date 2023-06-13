e8a69cc0c1de1b571e63ccf9d069bf4a525a366a
Corrado Caudek <corrado.caudek@unifi.it>
2023-06-13 18:30:00 +0200

    added bdi2.R

---
+++ bdi2.R
@@ -0,0 +1,23 @@
+#' \code{scoring_bdi2()} generates the total score of the BDI-2 Scale
+#' @param data.frame.
+#' @return data.frame.
+#' The first column has the subjects' ids; the following columns are the 58 
+#' items of the Tri-PM The items must be named `bdi2_1`, ... `bdi2_21`.
+scoring_bdi2 <- function(d) {
+  
+  # d <- rio::import(here("data", "prep", "quest_scales", "bdi2_items.csv"))
+  
+  # BDI-2 items.
+  bdi2_items <- d %>% 
+    dplyr::select(-user_id)
+  
+  d$bdi2_score <- rowSums(bdi2_items)
+  # hist(d$bdi2_score)
+  # mean(d$bdi2_score)
+  
+  # BDI-2 subscales and subject code
+  bdi2_scale <- d |> 
+    dplyr::select(user_id, bdi2_score)
+  
+  bdi2_scale
+}
